2231cd97d1b9f048234384467b1ab2a0d68e34ed
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-17 11:19:38 +0200

    add of the navbar app

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/home');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: const [
+        BottomNavigationBarItem(
+          label: 'Home',
+          icon: Icon(Icons.home),
+        ),
+        BottomNavigationBarItem(
+          label: 'List',
+          icon: Icon(Icons.list),
+        ),
+        BottomNavigationBarItem(
+          label: 'Add',
+          icon: Icon(Icons.add),
+        ),
+
+
+      ],
+
+    );
+  }
+}
+
+

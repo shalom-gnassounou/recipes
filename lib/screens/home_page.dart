6ac3fc925ce2672a9f0e0e034e7769dab5ff8ca9
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-17 12:09:29 +0200

    add of the recipe card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/widgets/nav_bar.dart';
+import 'package:recipes/widgets/recipe_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,15 +9,26 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Column(
-
+          children: [
+            SizedBox(height: 30,),
+            Center(
+              child: Text(
+                "Recipes",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            RecipeCard(title: "Un burger", imagePath: "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg", description: "Un bon burger")
+          ],
         ),
-
       ),
       bottomNavigationBar: NavBar(),
-
-
     );
   }
 }
---
+++ lib/widgets/recipe_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final String description;
+  final VoidCallback? onTap;
+
+  const RecipeCard({
+    Key? key,
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 12),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFC6A169),
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    description,
+                    style: const TextStyle(color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
